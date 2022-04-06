47d8e6b1a928075e0bbe023b618ecb4117be2b41
anjneya <anjneya.pathak2000@gmail.com>
2022-04-06 14:54:44 +0530

    first commit

---
+++ src/main/java/com/bharath/jaxb/jaxb/xjc/App.java
@@ -0,0 +1,13 @@
+package com.bharath.jaxb.jaxb.xjc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/anjneya/soap/api/SpringBootSoapWsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anjneya.soap.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSoapWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
